86ad1c15431e111df0a723552976578daa4e9aaa
Dannana Venkataramana <dvenkat507@gmail.com>
2020-05-23 20:12:09 +0530

    Venkat Update

--- app/src/main/java/com/example/height_finder/MainActivity.java
+++ app/src/main/java/com/example/height_finder/MainActivity.java
@@ -12,8 +12,10 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener
+{
     Switch Permission;
+    private static final int Permission_code = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -36,14 +38,14 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     {
         if(b)
         {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, Permission_code);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
     {
-        if (requestCode == 123)
+        if (requestCode == Permission_code)
         {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED)
             {
--- app/src/main/java/com/example/height_finder/MapsActivity.java
+++ app/src/main/java/com/example/height_finder/MapsActivity.java
@@ -37,7 +37,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener, View.OnClickListener
 {
     private static final String TAG ="MainActivity";
     private Button positon_marker,clear_marker;
@@ -82,71 +82,79 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         positon_marker = (Button)findViewById(R.id.btn_position_marker);
-        positon_marker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LatLng currentlocation = getLocation();
-                if (currentlocation != null) {
-                    if (marker_value == 0) {
-                        Log.d(TAG, "run: " + currentlocation.latitude + " " + currentlocation.longitude);
-                        BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.marker1);
-                        Bitmap b = bitmapdraw.getBitmap();
-                        marker1 = mMap.addMarker(new MarkerOptions().position(currentlocation).title("Marker in Current location").icon(BitmapDescriptorFactory.fromBitmap(b)));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentlocation, 16.0f));
-                        Resources res = getResources();
-                        positon_marker.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(res, R.drawable.marker2_xml, null), null, null, null);
-                        marker_value++;
-                        ready = false;
-                    } else if (marker_value == 1) {
-                        //LatLng pos = new LatLng(13.0723449, 77.5758208);
-                        BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.marker2);
-                        Bitmap b = bitmapdraw.getBitmap();
-                        marker2 = mMap.addMarker(new MarkerOptions().position(currentlocation).title("Marker in Current location").icon(BitmapDescriptorFactory.fromBitmap(b)));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentlocation, 16.0f));
-                        pline = mMap.addPolyline(new PolylineOptions()
-                                .add(marker1.getPosition())
-                                .add(marker2.getPosition()));
-                        distance = findDistance(marker1, marker2);
-                        bearing = findbearing(marker2, marker1);
-                        Resources res = getResources();
-                        positon_marker.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(res, R.drawable.cancelmarkerxml, null), null, null, null);
-                        positon_marker.setEnabled(false);
-                        marker_value++;
-                        if(distance <= 25) {
-                            minViewAngle = Math.toDegrees(Math.atan(7.5 / distance));
-                            ready = true;
-                        }
-                        else
-                        {
-                            Toast.makeText(getApplicationContext(), "Your are to far from building come close", Toast.LENGTH_LONG ).show();
-                        }
+        positon_marker.setOnClickListener(this);
+        clear_marker = (Button)findViewById(R.id.btn_clear_marker);
+        clear_marker.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if(view.equals(positon_marker))
+        {
+            LatLng currentlocation = getLocation();
+            if (currentlocation != null)
+            {
+                if (marker_value == 0)
+                {
+                    Log.d(TAG, "run: " + currentlocation.latitude + " " + currentlocation.longitude);
+                    BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.marker1);
+                    Bitmap b = bitmapdraw.getBitmap();
+                    marker1 = mMap.addMarker(new MarkerOptions().position(currentlocation).title("Marker in Current location").icon(BitmapDescriptorFactory.fromBitmap(b)));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentlocation, 16.0f));
+                    Resources res = getResources();
+                    positon_marker.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(res, R.drawable.marker2_xml, null), null, null, null);
+                    marker_value++;
+                    ready = false;
+                }
+                else if (marker_value == 1)
+                {
+                    //LatLng pos = new LatLng(13.0723449, 77.5758208);
+                    BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.marker2);
+                    Bitmap b = bitmapdraw.getBitmap();
+                    marker2 = mMap.addMarker(new MarkerOptions().position(currentlocation).title("Marker in Current location").icon(BitmapDescriptorFactory.fromBitmap(b)));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentlocation, 16.0f));
+                    pline = mMap.addPolyline(new PolylineOptions()
+                            .add(marker1.getPosition())
+                            .add(marker2.getPosition()));
+                    distance = findDistance(marker1, marker2);
+                    bearing = findbearing(marker2, marker1);
+                    Resources res = getResources();
+                    positon_marker.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(res, R.drawable.cancelmarkerxml, null), null, null, null);
+                    positon_marker.setEnabled(false);
+                    marker_value++;
+                    if((distance <= 25) && (distance > 0))
+                    {
+                        minViewAngle = Math.toDegrees(Math.atan(7.5 / distance));
+                        ready = true;
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "Your are to far from building come close", Toast.LENGTH_LONG ).show();
                     }
                 }
             }
-        });
-        clear_marker = (Button)findViewById(R.id.btn_clear_marker);
-        clear_marker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(marker_value == 2)
-                {
-                    marker2.remove();
-                    marker_value--;
-                    positon_marker.setEnabled(true);
-                    Resources res = getResources();
-                    positon_marker.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(res, R.drawable.marker2_xml, null), null, null, null);
-                    pline.remove();
-                }
-                else if(marker_value == 1)
-                {
-                    marker1.remove();
-                    Resources res = getResources();
-                    positon_marker.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(res, R.drawable.marker1_xml, null), null, null, null);
-                    marker_value--;
-                }
-                ready = false;
+        }
+        else if(view.equals(clear_marker))
+        {
+            if(marker_value == 2)
+            {
+                marker2.remove();
+                marker_value--;
+                positon_marker.setEnabled(true);
+                Resources res = getResources();
+                positon_marker.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(res, R.drawable.marker2_xml, null), null, null, null);
+                pline.remove();
             }
-        });
+            else if(marker_value == 1)
+            {
+                marker1.remove();
+                Resources res = getResources();
+                positon_marker.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(res, R.drawable.marker1_xml, null), null, null, null);
+                marker_value--;
+            }
+            ready = false;
+        }
     }
 
     @Override
@@ -430,7 +438,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mSnrCn0UsedAvg = cn0UsedSum / svUsedCount;
             }
         }
-
     }
 
     private void ParseSatelliteData()
@@ -446,6 +453,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 len = pAzims.length;
             Integer[] interscetion = new Integer[len];
             interscetion = Intersection();
+            pAzims = new float[mAzims.length];
+            pPrns = new Integer[mPrns.length];
+            pElevs = new float[mElevs.length];
+            for(int i = 0; i < mPrns.length; i++)
+            {
+
+            }
         }
         else
         {
@@ -462,4 +476,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Integer[] result = s1.toArray(new Integer[s1.size()]);
         return result;
     }
+
+
 }
