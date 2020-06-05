810bf817657d9afe2c37a03daf5ce45d0b5ba631
HrishikeshSD <contact.me.at.hrishikesh@gmail.com>
2020-06-05 20:53:34 +0530

    After Android update final

--- app/src/main/java/com/example/height_finder/MainActivity.java
+++ app/src/main/java/com/example/height_finder/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
